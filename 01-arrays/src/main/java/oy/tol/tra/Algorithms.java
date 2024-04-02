8b4b7ac6451b4d3bc09e03175ecc30b9646bb923
Yang Haojia <3266282553@qq.com>
2024-04-02 15:00:46 +0800

    DSA 4.2

--- 01-arrays/src/main/java/oy/tol/tra/Algorithms.java
+++ 01-arrays/src/main/java/oy/tol/tra/Algorithms.java
@@ -1,36 +1,27 @@
 package oy.tol.tra;
 
-public class Algorithms<T extends Comparable<T>> {
-
-    private T[] array = null;
-
-    public Algorithms(T[] array) {
-        this.array = array.clone();
+public class Algorithms {
+    private Algorithms() {
+        // Empty
     }
 
-    public void reverse() {
-        int i = 0;
-        while (i < array.length/2) {
-            T temp = array[i];
-            array[i] = array[array.length-i-1];
-            array[array.length-i-1] = temp;
-            i++;
-        }
-    }
-
-    public void sort() {
-        for (int i = 1; i < array.length; i++) {
-            T key = array[i];
-            int j = i - 1;
-            while (j >= 0 && array[j].compareTo(key) > 0) {
-                array[j + 1] = array[j];
-                j = j - 1;
+    public static <T extends Comparable<T>> void sort(T[] array) {
+        for (int i = 0; i < array.length - 1; i++) {
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[i].compareTo(array[j]) > 0) {
+                    T temp = array[i];
+                    array[i] = array[j];
+                    array[j] = temp;
+                }
             }
-            array[j + 1] = key;
         }
     }
 
-    public T[] getArray() {
-        return array;
+    public static <T> void reverse(T[] array) {
+        for (int i = 0; i < array.length / 2; i++) {
+            T temp = array[i];
+            array[i] = array[array.length - 1 - i];
+            array[array.length - 1 - i] = temp;
+        }
     }
 }
--- 01-arrays/src/main/java/oy/tol/tra/Grades.java
+++ 01-arrays/src/main/java/oy/tol/tra/Grades.java
@@ -1,22 +1,59 @@
 package oy.tol.tra;
 
-public class Grades<T extends Comparable<T>> {
+/**
+ * A simple array of student grades to be used in testing
+ * misbehaving algorithm for reversing the array.
+ */
+public class Grades {
+   
+   private Integer [] grades = null;
 
-   private Algorithms<T> algorithms;
-
-   public Grades(T[] grades) {
-      this.algorithms = new Algorithms<>(grades);
+   /**
+    * A constructor for building IntArrays.
+    * @param grades the plain Java integer array with numbers to add.
+    */
+   public Grades(Integer [] grades) {
+      this.grades = new Integer [grades.length];
+      for (int counter = 0; counter < grades.length; counter++) {
+         this.grades[counter] = grades[counter];
+      }
    }
 
+   /**
+    * The method to reverse the internal Java int array.
+    */
    public void reverse() {
-      algorithms.reverse();
+      /* TODO:
+       1. Edit the test data files to see if the reverse() really works or not.
+       2. Execute the IntArrayTests to see that some of them fail.
+       3. Study the code below and try to find what is the issue.
+       4. Use the debugger to see the execution and variable values if necessary.
+       5. Fix the issue.
+       6. Transform the algorithm to <strong>use</strong> the generic one from Algorithms.java, as instructed in the readme file.
+      */
+     Algorithms.reverse(grades);
    }
 
+   /**
+    * Sorts the array to ascending order.
+    */
    public void sort() {
-      algorithms.sort();
+      /* TODO:
+       1. Edit the test data files to see if the sort() really works or not.
+       2. Execute the IntArrayTests to see that some of them fail.
+       3. Study the code below and try to find what is the issue.
+       4. Use the debugger to see the execution and variable values if necessary.
+       5. Fix the issue.
+       6. Transform the algorithm to <strong>use</strong> the generic one from Algorithms.java as instructed in the readme file.
+      */
+      Algorithms.sort(grades);
    }
 
-   public T[] getArray() {
-      return algorithms.getArray();
+   /**
+    * Returns the plain Java int [] array for investigation.
+    * @return The int array.
+    */
+   public Integer [] getArray() {
+      return grades;
    }
 }
